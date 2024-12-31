4cc8a06e52ec96c92ad60f06b572a55d72bfb910
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-12-31 11:28:15 -0600

    Move library modules to src/module/lib/

--- src/module/index.ts
+++ src/module/index.ts
@@ -1,10 +1,10 @@
-import Database from "./Database.js";
-import DBTable from "./Table.js";
-import DBColumn from "./Column.js";
+import Database from "./lib/Database.js";
+import DBTable from "./lib/Table.js";
+import DBColumn from "./lib/Column.js";
 import {
   DataType as DBTDT,
   TableColumnSettings as DBTTCS,
-} from "./DatabaseTypes.js";
+} from "./lib/DatabaseTypes.js";
 
 export type DataType = DBTDT;
 export type TableColumnSettings = DBTTCS;
